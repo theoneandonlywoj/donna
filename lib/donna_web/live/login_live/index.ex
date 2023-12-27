9d4685cba564004758b59b25a2fa2ff47b23a4ed
theoneandonlywoj <theoneandonlywoj@gmail.com>
2023-12-27 02:10:42 +0000

    LoginLiveView: Index.ex + Index.html.heex.

---
+++ lib/donna_web/live/login_live/index.ex
@@ -0,0 +1,18 @@
+defmodule DonnaWeb.LoginLive.Index do
+  use DonnaWeb, :live_view
+
+  @impl true
+  def mount(_params, _session, socket) do
+    {:ok, socket}
+  end
+
+  @impl true
+  def handle_params(_params, _url, socket) do
+    {:noreply, socket}
+  end
+
+  @impl true
+  def handle_event("event_name", _payload, socket) do
+    {:noreply, socket}
+  end
+end
